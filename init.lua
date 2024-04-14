d119418bc2a50939379299abf17ab52a004e7b74
Pato Lankenau <rust@p.lankenau.io>
2024-04-14 23:08:05 +0100

    Lualine and TODO plugins

--- init.lua
+++ init.lua
@@ -565,6 +565,19 @@ require("lazy").setup({
 			})
 		end,
 	},
+
+	{ -- lualine display
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
+
+	{ -- display TODOs prominently
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
 })
 
 -- vim: ts=2 sts=2 sw=2 et
