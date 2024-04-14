1a6fc26d8921720685eea1383c3d0e2573956994
Pato Lankenau <rust@p.lankenau.io>
2024-04-14 23:07:47 +0100

    Add gitsigns to see lines added/removed

--- init.lua
+++ init.lua
@@ -107,6 +107,19 @@ require("lazy").setup({
 		end,
 	},
 
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+
 	{ -- Rust Analyzer plugin for better support
 		"mrcjkb/rustaceanvim",
 		version = "^4", -- Recommended
