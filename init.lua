50145c0c80f244a0ce8fc3710d4b301e57957828
Pato Lankenau <rust@p.lankenau.io>
2024-05-31 09:25:04 +0100

    Add trouble
    Now that it has been fully rewritten

--- init.lua
+++ init.lua
@@ -816,6 +816,44 @@ require("lazy").setup({
 			vim.api.nvim_set_keymap("n", "<leader>fc", "<cmd>lua require('dropbar.api').pick()<CR>", { silent = true })
 		end,
 	},
+
+	{ -- pretty diagnostics, references, etc..
+		"folke/trouble.nvim",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+		cmd = "Trouble",
+		opts = {},
+	},
 })
 
 -- vim: ts=2 sts=2 sw=2 et
