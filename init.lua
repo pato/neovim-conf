591ba6b1fb0f2d0201dea7723624bbc37a990874
Pato Lankenau <rust@p.lankenau.io>
2024-07-18 09:54:28 +0100

    New which key!

--- init.lua
+++ init.lua
@@ -1021,6 +1021,23 @@ require("lazy").setup({
 		event = "VeryLazy",
 		config = true,
 	},
+
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {
+			preset = "helix",
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
 })
 
 -- vim: ts=2 sts=2 sw=2 et
